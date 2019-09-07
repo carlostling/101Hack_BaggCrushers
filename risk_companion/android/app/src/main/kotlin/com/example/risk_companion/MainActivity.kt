56ba883738aaaf87b9a42090d91c648c492e58d0
henriklagergren <henriklagergren@hotmail.com>
2019-09-07 10:20:53 +0200

    Created new flutter project

---
+++ risk_companion/android/app/src/main/kotlin/com/example/risk_companion/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.risk_companion
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
