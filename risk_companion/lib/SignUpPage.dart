5ebc441ba030706d85c33a19008d98272aaded47
henriklagergren <henriklagergren@hotmail.com>
2019-09-07 15:06:02 +0200

    Fixed onChange error

--- risk_companion/lib/SignUpPage.dart
+++ risk_companion/lib/SignUpPage.dart
@@ -5,11 +5,10 @@ import 'ScreenUtils.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignUpPage extends StatelessWidget {
-
-  int _age;
-  int _focus;
+  String _age;
+  String _focus;
   String _carLicense;
-  int _kmPerYear;
+  String _kmPerYear;
   String _destination;
 
   @override
@@ -42,7 +41,7 @@ class SignUpPage extends StatelessWidget {
           ),
           InputFieldWidget(
             title: "Ålder",
-            onChange: _age,
+            callback: changeAge,
           ),
           SizedBox(
             height: 10,
@@ -50,6 +49,7 @@ class SignUpPage extends StatelessWidget {
           ),
           InputFieldWidget(
             title: "Nummerplåt",
+            callback: changeCarLicense,
           ),
           SizedBox(
             height: 10,
@@ -57,62 +57,102 @@ class SignUpPage extends StatelessWidget {
           ),
           InputFieldWidget(
             title: "Km per år",
+            callback: changeKmPerYear,
           ),
           SizedBox(
             height: 10,
             width: 0,
           ),
-          Text("Nuvarande fokus:",style: TextStyle(fontSize: ScreenUtils.getFontSize(20),fontWeight: FontWeight.w500),),
+          Text(
+            "Nuvarande fokus:",
+            style: TextStyle(
+                fontSize: ScreenUtils.getFontSize(20),
+                fontWeight: FontWeight.w500),
+          ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: ScreenUtils.getWidth(50)),
             child: FocusSliderWidget(),
           ),
           InputFieldWidget(
             title: "Slutdestination",
+            callback: changeDestination,
           ),
-        Expanded(child: Container(),), 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Checkbox(
-              value: true,
+          Expanded(
+            child: Container(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Checkbox(
+                value: true,
+              ),
+              Text(
+                "Spara min data till nästa riskanalys",
+                style: TextStyle(fontWeight: FontWeight.w400),
+              )
+            ],
+          ),
+          RaisedButton(
+            onPressed: () => _submit(),
+            color: Color.fromRGBO(227, 6, 19, 0.8),
+            shape: StadiumBorder(),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Gå vidare",
+                style: TextStyle(
+                    color: Colors.white, fontSize: ScreenUtils.getFontSize(20)),
+              ),
             ),
-            Text("Spara min data till nästa riskanalys",style: TextStyle(fontWeight: FontWeight.w400),)
-          ],
-        ),
-        RaisedButton(
-          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage())),
-          color: Color.fromRGBO(227, 6, 19, 0.8),
-          shape: StadiumBorder(),
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text("Gå vidare",style: TextStyle(color: Colors.white,fontSize: ScreenUtils.getFontSize(20)),),
           ),
-        ),
-        SizedBox(height: ScreenUtils.getHeight(20),),
+          SizedBox(
+            height: ScreenUtils.getHeight(20),
+          ),
         ],
       ),
     );
   }
 
-  bool _submit(){
+  void changeAge(String newValue) {
+    _age = newValue;
+  }
 
+  void changeFocus(String newValue) {
+    _focus = newValue;
+  }
+
+  void changeCarLicense(String newValue) {
+    _carLicense = newValue;
+  }
+
+  void changeKmPerYear(String newValue) {
+    _kmPerYear = newValue;
+  }
+
+  void changeDestination(String newValue) {
+    _destination = newValue;
+  }
+
+  void _submit() {
+    Profile profile = new Profile(_age,_carLicense,_destination,_focus,_kmPerYear);
+    
+    MaterialPageRoute(builder: (context) => ResultPage());
   }
 }
 
 class InputFieldWidget extends StatelessWidget {
   final String title;
-  final String value;
-  final dynamic onChange;
+  final String initialValue;
+  Function callback;
 
-  InputFieldWidget({@required this.title,this.value,this.onChange});
+  InputFieldWidget({@required this.title, this.initialValue, this.callback});
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: ScreenUtils.getWidth(15)),
       child: TextFormField(
-        initialValue: value,
+        initialValue: initialValue,
         decoration: new InputDecoration(
           labelStyle: TextStyle(color: Colors.grey),
           labelText: title,
@@ -122,8 +162,8 @@ class InputFieldWidget extends StatelessWidget {
             borderSide: new BorderSide(),
           ),
         ),
-        onChanged: (string){
-          onChange = string;
+        onChanged: (string) {
+          callback(string);
         },
         validator: (val) {
           if (val.length == 0) {
@@ -134,7 +174,7 @@ class InputFieldWidget extends StatelessWidget {
         },
         keyboardType: TextInputType.text,
         style: new TextStyle(
-          color: Colors.white,
+          color: Colors.black,
           fontFamily: "Poppins",
         ),
       ),
@@ -152,30 +192,28 @@ class _FocusSliderWidgetState extends State<FocusSliderWidget> {
   @override
   Widget build(BuildContext context) {
     return SliderTheme(
-              data: SliderTheme.of(context).copyWith(
-                activeTrackColor: Colors.lightBlue[200],
-                inactiveTrackColor: Colors.grey[200],
-                trackHeight: 7.0,
-                thumbColor: Colors.grey[400],
-                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 8.0),
-                tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 0),
-                overlayColor: Colors.lightBlue[200].withAlpha(10),
-                overlayShape: RoundSliderOverlayShape(overlayRadius: 14.0),
-              ),
-              child: Slider(
-                min: 1.0,
-                max: 5.0,
-                divisions: 7,
-                value: _sliderValue.toDouble(),
-                label: '${_sliderValue.round()} av 5',
-                onChanged: (double currentSizeOfQuiz) {
-
-                    setState(() {
-                      _sliderValue = currentSizeOfQuiz.toInt();
-                    });
-                },
-              ),
-            );
+      data: SliderTheme.of(context).copyWith(
+        activeTrackColor: Colors.lightBlue[200],
+        inactiveTrackColor: Colors.grey[200],
+        trackHeight: 7.0,
+        thumbColor: Colors.grey[400],
+        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 8.0),
+        tickMarkShape: RoundSliderTickMarkShape(tickMarkRadius: 0),
+        overlayColor: Colors.lightBlue[200].withAlpha(10),
+        overlayShape: RoundSliderOverlayShape(overlayRadius: 14.0),
+      ),
+      child: Slider(
+        min: 1.0,
+        max: 5.0,
+        divisions: 7,
+        value: _sliderValue.toDouble(),
+        label: '${_sliderValue.round()} av 5',
+        onChanged: (double currentSizeOfQuiz) {
+          setState(() {
+            _sliderValue = currentSizeOfQuiz.toInt();
+          });
+        },
+      ),
+    );
   }
 }
-
