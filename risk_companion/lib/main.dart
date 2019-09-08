0a92ea710d1ab1907389fe7f42ac4cbdaa3671c1
henriklagergren <henriklagergren@hotmail.com>
2019-09-08 12:48:26 +0200

    Fixed orientation

--- risk_companion/lib/main.dart
+++ risk_companion/lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-     
+     SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(fontFamily: 'Quicksand'),
